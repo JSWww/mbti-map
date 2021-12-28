168471707844908cb295ed7099cbce3069571b5b
waterrr17 <mj0904meeju@naver.com>
2021-12-28 14:51:20 +0900

    REFACTOR: move form into ModalWindow

--- src/components/AddPerson.vue
+++ src/components/AddPerson.vue
@@ -1,37 +1,7 @@
 <template>
 	<div>
 		<button @click="showModal">Add</button>
-		<ModalWindow v-if="modalOn" @close="closeModal">
-			<button @click="closeModal" class="close">X</button>
-			<br />
-			<form v-on:submit.prevent="doAdd">
-				Name <input type="text" class="info" v-model="name" />
-				<br />
-				MBTI
-				<select class="info" v-model="mbti">
-					<option>ESTJ</option>
-					<option>ESTP</option>
-					<option>ESFJ</option>
-					<option>ESFP</option>
-					<option>ENTJ</option>
-					<option>ENTP</option>
-					<option>ENFJ</option>
-					<option>ENFP</option>
-					<option>ISTJ</option>
-					<option>ISTP</option>
-					<option>ISFJ</option>
-					<option>ISFP</option>
-					<option>INTJ</option>
-					<option>INTP</option>
-					<option>INFJ</option>
-					<option>INFP</option>
-				</select>
-				<br />
-				Photo <input type="file" class="info" v-on:change="uploadPhoto" />
-				<div v-if="img"><img class="preview" v-bind:src="img" /></div>
-				<br />
-				<button type="submit" class="info">Add</button>
-			</form>
+		<ModalWindow v-if="modalOn" @close="closeModal" @doAdd="doAdd">
 		</ModalWindow>
 	</div>
 </template>
@@ -42,7 +12,6 @@ export default {
 	components: {
 		ModalWindow,
 	},
-	name: "#addPerson",
 	data: () => {
 		return {
 			modalOn: false,
@@ -55,8 +24,8 @@ export default {
 		showModal: function () {
 			this.modalOn = true;
 		},
-		doAdd: function () {
-			this.$emit("addPersonProfile", this.name, this.mbti, this.img);
+		doAdd: function (name, mbti, img) {
+			this.$emit("addPersonProfile", name, mbti, img);
 			this.modalOn = false;
 			this.name = "";
 			this.mbti = "";
@@ -68,27 +37,6 @@ export default {
 			this.mbti = "";
 			this.img = "";
 		},
-		uploadPhoto: function (event) {
-			const file = event.target.files[0];
-			if (file && file.type.match(/^image\/(png|jpeg)$/)) {
-				this.img = window.URL.createObjectURL(file);
-			}
-		},
 	},
 };
 </script>
-
-<style scoped>
-.preview {
-	width: 50px;
-	height: 50px;
-}
-.close {
-	float: right;
-	border: 0em;
-	background: rgba(0, 0, 0, 0);
-}
-.info {
-	margin-bottom: 2em;
-}
-</style>
--- src/components/PersonProfile.vue
+++ src/components/PersonProfile.vue
@@ -15,38 +15,17 @@
 				<label>{{ name }} {{ mbti }}</label>
 			</li>
 		</ul>
-		<ModalWindow v-if="modalOn" @close="closeModal">
-			<button @click="closeModal" class="close">X</button>
-			<br />
-			<form v-on:submit.prevent="doUpdate(name, mbti, img, id)">
-				Name <input type="text" class="info" v-model="name" />
-				<br />
-				MBTI
-				<select class="info" v-model="mbti">
-					<option>ESTJ</option>
-					<option>ESTP</option>
-					<option>ESFJ</option>
-					<option>ESFP</option>
-					<option>ENTJ</option>
-					<option>ENTP</option>
-					<option>ENFJ</option>
-					<option>ENFP</option>
-					<option>ISTJ</option>
-					<option>ISTP</option>
-					<option>ISFJ</option>
-					<option>ISFP</option>
-					<option>INTJ</option>
-					<option>INTP</option>
-					<option>INFJ</option>
-					<option>INFP</option>
-				</select>
-				<br />
-				Photo <input type="file" class="info" v-on:change="uploadPhoto" />
-				<div v-if="img"><img class="preview" v-bind:src="img" /></div>
-				<br />
-				<button type="submit" class="info">Update</button>
-				<button @click="doRemove(id)">Delete</button>
-			</form>
+		<ModalWindow
+			v-if="modalOn"
+			@close="closeModal"
+			@doUpdate="doUpdate"
+			@doRemove="doRemove"
+			:getId="id"
+			:getName="name"
+			:getMbti="mbti"
+			:getImg="img"
+			:isUpdate="isUpdate"
+		>
 		</ModalWindow>
 	</section>
 </template>
@@ -60,23 +39,23 @@ export default {
 	components: {
 		ModalWindow,
 	},
-	name: "#personProfile",
-	data: () => {
+	data() {
 		return {
 			modalOn: false,
+			id: 0,
 			name: "",
 			mbti: "",
 			img: "",
-			id: 0,
+			isUpdate: true,
 		};
 	},
 	methods: {
 		showModal: function (name, img, mbti, id) {
-			this.modalOn = true;
 			this.name = name;
 			this.img = img;
 			this.mbti = mbti;
 			this.id = id;
+			this.modalOn = true;
 		},
 		doUpdate: function (name, mbti, img, id) {
 			this.$emit("updatePersonProfile", name, mbti, img, id);
@@ -86,19 +65,16 @@ export default {
 			this.img = "";
 		},
 		doRemove: function (id) {
-			this.$emit("removePersonProfile", id);
+			let remove = confirm("Delete?");
+			if (remove) {
+				this.$emit("removePersonProfile", id);
+			}
 		},
 		closeModal: function () {
 			this.modalOn = false;
 			this.name = "";
 			this.mbti = "";
 			this.img = "";
-		},
-		uploadPhoto: function (event) {
-			const file = event.target.files[0];
-			if (file && file.type.match(/^image\/(png|jpeg)$/)) {
-				this.img = window.URL.createObjectURL(file);
-			}
 		},
 	},
 };
@@ -109,17 +85,5 @@ export default {
 	width: 50px;
 	height: 50px;
 	border-radius: 50%;
-}
-.preview {
-	width: 50px;
-	height: 50px;
-}
-.close {
-	float: right;
-	border: 0em;
-	background: rgba(0, 0, 0, 0);
-}
-.info {
-	margin-bottom: 2em;
 }
 </style>
