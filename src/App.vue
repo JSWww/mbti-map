b8fde824f28961887ac49bb1014ec9c10462ba82
JSWww <tjddnjs9497@naver.com>
2021-12-30 11:46:13 +0900

    ADD: env file

--- src/App.vue
+++ src/App.vue
@@ -30,12 +30,12 @@ import "firebase/compat/firestore";
 import "firebase/compat/storage";
 
 const config = {
-	apiKey: "AIzaSyA2VETiMU4LsgdWu0XdtEvQJ8MMT28FU5M",
-	authDomain: "mbtimap-dad6b.firebaseapp.com",
-	projectId: "mbtimap-dad6b",
-	storageBucket: "mbtimap-dad6b.appspot.com",
-	messagingSenderId: "457163912484",
-	appId: "1:457163912484:web:61fed987eff1c57d3defb1",
+	apiKey: process.env.VUE_APP_FIREBASE_APIKEY,
+	authDomain: process.env.VUE_APP_FIREBASE_AUTHDOMAIN,
+	projectId: process.env.VUE_APP_FIREBASE_PROJECTID,
+	storageBucket: process.env.VUE_APP_FIREBASE_STORAGEBUCKET,
+	messagingSenderId: process.env.VUE_APP_FIREABSE_MESSAGINGSENDERID,
+	appId: process.env.VUE_APP_FIREBASE_APPID,
 };
 
 firebase.initializeApp(config);
