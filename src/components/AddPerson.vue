13bfa30805b438c5228772f9473ba9398615e298
waterrr17 <mj0904meeju@naver.com>
2021-12-31 13:18:33 +0900

    ADD:add btn css

--- src/components/AddPerson.vue
+++ src/components/AddPerson.vue
@@ -40,3 +40,26 @@ export default {
 	},
 };
 </script>
+
+<style>
+button {
+	padding: 1.3em 3em;
+	font-size: 12px;
+	text-transform: uppercase;
+	letter-spacing: 2.5px;
+	font-weight: 500;
+	color: rgba(25, 43, 77);
+	background-color: #fff;
+	border: none;
+	border-radius: 45px;
+	box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+	transition: all 0.3s ease 0s;
+	cursor: pointer;
+	outline: none;
+}
+
+button:hover {
+	background-color: rgba(25, 43, 77);
+	color: #fff;
+}
+</style>
